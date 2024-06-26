1cd746fd14c2890863b56ccc98d57495aa1e634f
TBargos <bargos.work@gmail.com>
2024-06-26 16:11:17 +0300

    added buttons for clear tasks and adding new tasks, proccessing post queries

--- todo_manager/main/services.py
+++ todo_manager/main/services.py
@@ -1,5 +1,7 @@
 """Обслуживающие функции для приложения main"""
+from django.shortcuts import get_object_or_404
 from main.models import Task
+
 
 def load_actual_tasks():
     """Загружает активные задачи"""
@@ -8,3 +10,18 @@ def load_actual_tasks():
 def load_completed_tasks():
     """Загружает завершённые задачи"""
     return Task.objects.filter(status=False)
+
+def add_new_task(task: str) -> None:
+    """Добавляет новую задачу"""
+    if isinstance(task, str):
+        Task.objects.create(taskname=task)
+
+def mark_task_finished(task_id):
+    """Убирает задачу в список завершённых"""
+    task = get_object_or_404(Task, id=task_id)
+    task.status = False
+    task.save()
+    
+def del_finished_tasks():
+    """Удаляет завершённые задачи из БД"""
+    Task.objects.all().filter(status=False).delete()
--- todo_manager/main/views.py
+++ todo_manager/main/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from django.views.decorators.csrf import csrf_exempt
 
-from main.services import load_actual_tasks, load_completed_tasks
+from main.services import *
 
 # Create your views here.
 
+@csrf_exempt
 def index(request):
     actual_tasks = load_actual_tasks()
     completed_tasks = load_completed_tasks()
@@ -13,5 +15,15 @@ def index(request):
         "actual_tasks": actual_tasks,
         "completed_tasks": completed_tasks
     }
+    
+    if request.POST:
+        if request.POST.get('newtask'):
+            add_new_task(request.POST.get('newtask'))
+        if request.POST.get('taskoff'):
+            mark_task_finished(request.POST.get('taskoff'))
+        if request.POST.get('clear_finished_tasks'):
+            del_finished_tasks()
+        return redirect(request.build_absolute_uri())
+    
     
     return render(request, 'main/index.html', context)
